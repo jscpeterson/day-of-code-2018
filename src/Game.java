33809cd2a2d5c3d22a2f6b1fd5d7210b8013446f
Joseph Peterson <jscpeterson@gmail.com>
2018-12-01 14:03:49 -0700

    Working implementation

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,8 @@ import java.util.Random;
 public class Game {
 
   Random rng = new Random();
+  public static final int ROWS = 20;
+  public static final int COLUMNS = 20;
 
   // 2D orthogonal universe
   // given an initial generation called a seed
@@ -12,16 +14,34 @@ public class Game {
   // Cell - Object based
 
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Game game = new Game();
-    VonNeumann neighbors = new VonNeumann();
-    Cell[][] grid = new Cell[5][5];
+    Cell[][] grid = new Cell[ROWS][COLUMNS];
     game.populateAll(grid);
+    printGrid(grid);
+    while (true) {
+      System.out.println();
+      grid = game.updateGrid(grid);
+      printGrid(grid);
+      Thread.sleep(1000);
+    }
+
+  }
+
+  public static Cell[][] updateGrid(Cell[][] grid) {
+    Cell[][] newGrid = new Cell[ROWS][COLUMNS];
+    for (int rows = 0; rows < ROWS; rows++) {
+      for (int columns = 0; columns < COLUMNS; columns++) {
+        newGrid[rows][columns] = grid[rows][columns].phaseOfLife(grid);
+      }
+    }
+    return newGrid;
+  }
+
+  public static void printGrid(Cell[][] grid) {
     for (Cell[] column : grid) {
       System.out.println(Arrays.toString(column));
     }
-    System.out.println();
-    System.out.println(Arrays.toString(grid[3][3].getNeighbors(grid)));
   }
 
   public void populateAll(Cell[][] grid) {
@@ -33,6 +53,22 @@ public class Game {
   }
 
   public class Cell {
+
+    public int getRow() {
+      return row;
+    }
+
+    public void setRow(int row) {
+      this.row = row;
+    }
+
+    public int getColumn() {
+      return column;
+    }
+
+    public void setColumn(int column) {
+      this.column = column;
+    }
 
     int row;
     int column;
@@ -47,21 +83,32 @@ public class Game {
     @Override
     public String toString() {
       if (alive) return "O";
-      else return "X";
+      else return " ";
     }
 
     Cell[] getNeighbors(Cell[][] grid) {
-      Cell[] neighbors = new Cell[]{
-          grid[row-1][column], //north
-          grid[row][column+1], //east
-          grid[row][column-1], //west
-          grid[row+1][column] //south
-      };
+      Cell[] neighbors = new Cell[8];
+      neighbors[0] = normalize(this, new Cell(-1, -1, true), grid); //northwest
+      neighbors[1] = normalize(this, new Cell(-1,  0, true), grid); //north
+      neighbors[2] = normalize(this, new Cell(-1,  1, true), grid);//northeast
+      neighbors[3] = normalize(this, new Cell( 0,  1, true), grid); //east
+      neighbors[4] = normalize(this, new Cell( 1,  1, true), grid); //southeast
+      neighbors[5] = normalize(this, new Cell( 1,  0, true), grid); //south
+      neighbors[6] = normalize(this, new Cell( 1, -1, true), grid);; //southwest
+      neighbors[7] = normalize(this, new Cell( 0, -1, true), grid);; //west
       //FIXME corners
       return neighbors;
     }
 
-    void phaseOfLife(Cell[][] grid) {
+    private Cell normalize(Cell base, Cell offset, Cell[][] grid) {
+      //assumes parameters are never smaller than -grid.length
+      int row = (base.getRow() + offset.getRow() + grid.length) % grid.length;
+      int column = (base.getColumn() + offset.getColumn() + grid.length) % grid.length;
+      return new Cell(row, column, grid[row][column].alive);
+    }
+
+    Cell phaseOfLife(Cell[][] grid) {
+      Cell newCell = new Cell(this.getRow(), this.getColumn(), this.alive);
       Cell[] neighbors = this.getNeighbors(grid);
       int numberOfLiveNeighbors = 0;
       for (Cell neighbor : neighbors) {
@@ -72,59 +119,24 @@ public class Game {
       if (this.alive) {
         // A live cell having less than 2 live neighbors dies
           if (numberOfLiveNeighbors < 2) {
-            this.alive = false;
+            newCell.alive = false;
           }
         // A live cell having 2 or 3 live neighbors lives on
           else if (numberOfLiveNeighbors == 2 || numberOfLiveNeighbors == 3) {
-            this.alive = true;
+            newCell.alive = true;
           }
         // A live cell having more than three live neighbors dies
         else if (numberOfLiveNeighbors > 3) {
-           this.alive = false;
+           newCell.alive = false;
           }
       } else {
         // A dead cell having 3 live neighbors becomes alive
         if (numberOfLiveNeighbors == 3) {
-          this.alive = true;
+          newCell.alive = true;
         }
       }
+      return newCell;
     }
-
-
-
-  }
-
-  public static class Location {
-    private final int row;
-    private final int column;
-
-    public Location(int row, int column) {
-      this.row = row;
-      this.column = column;
-    }
-
-    public int getRow() {
-      return row;
-    }
-
-    public int getColumn() {
-      return column;
-    }
-
-  }
-
-  public static class VonNeumann {
-
-    private final Location[] neighbors = {
-                          new Location(-1, 0),
-        new Location(0, -1),                   new Location(0, 1),
-                          new Location(1,  0)
-    };
-
-    public Location[] getNeighbors() {
-      return neighbors;
-    }
-
   }
 
 
